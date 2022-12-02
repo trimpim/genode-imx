5fdad7f1572da5f932bb6883881f31671f343775
Jean-Adrien DOMAGE <jean-adrien.domage@gapfruit.com>
2022-12-02 22:44:27 +0100

    sd_card: update imx8mm target.mk
    This commits update the target.mk of the imx8mm/ variant of the driver
    to comply with the latest change of the build settings of the driver.

--- src/drivers/sd_card/imx8mm/target.mk
+++ src/drivers/sd_card/imx8mm/target.mk
@@ -6,7 +6,6 @@ LIBS     = base
 SRC_CC  += main.cc
 SRC_CC  += time.cc
 SRC_CC  += lx_emul/shared_dma_buffer.cc
-
 SRC_C   += block.c
 SRC_C   += dummies.c
 SRC_C   += generated_dummies.c
@@ -33,4 +32,4 @@ BOARDS                       := imx8mm_iot_gate
 DTS_PATH(imx8mm_iot_gate)    := arch/arm64/boot/dts/compulab/sb-iotgimx8.dts
 DTS_EXTRACT(imx8mm_iot_gate) := --select usdhc3 usdhc2
 
-include $(REP_DIR)/src/drivers/linux_mnt_reform2_drv_target.inc
+include $(REP_DIR)/src/drivers/linux_mnt_reform2_drv_target_arm_v8.inc
