e3e4af962e88e7f346d92c3651e29f13d8a3356e
Pirmin Duss <pirmin.duss@gapfruit.com>
2023-06-06 12:42:17 +0200

    usb_host: add driver for the imx8mm SoC
    To simplify the integration of the usb_host driver in complex scenarios
    it controls both USB host controllers of the SoC.
    
    Issue gapfruit#1588, gapfruit#1668

---
+++ recipes/src/imx8mm_usb_host_drv/content.mk
@@ -0,0 +1,11 @@
+DRIVER := usb_host/imx8mm
+
+MIRROR_FROM_OS_DIR := src/lib/genode_c_api/usb.cc
+
+content: $(MIRROR_FROM_OS_DIR)
+
+$(MIRROR_FROM_OS_DIR):
+	mkdir -p $(dir $@)
+	cp -r $(GENODE_DIR)/repos/os/$@ $@
+
+include $(REP_DIR)/recipes/src/linux_mnt_reform2_drv_content.inc
---
+++ src/drivers/usb_host/imx8mm/target.mk
@@ -0,0 +1,32 @@
+TARGET  := imx8mm_usb_host
+REQUIRES = arm_v8a
+LIBS     = base
+INC_DIR  = $(PRG_DIR)
+SRC_CC  += main.cc
+SRC_CC  += time.cc
+SRC_CC  += lx_emul/shared_dma_buffer.cc
+SRC_C   += dummies.c
+SRC_C   += lx_emul.c
+SRC_C   += lx_emul/usb.c
+SRC_C   += $(notdir $(wildcard $(PRG_DIR)/generated_dummies.c))
+
+CC_OPT_drivers/usb/chipidea/trace  += -I$(LX_CONTRIB_DIR)/drivers/usb/chipidea -DCONFIG_USB_CHIPIDEA_HOST
+CC_OPT_drivers/usb/chipidea/host   += -DCONFIG_USB_CHIPIDEA_HOST
+CC_OPT_drivers/usb/host/xhci-trace += -I$(LX_CONTRIB_DIR)/drivers/usb/host
+CC_OPT_generated_dummies           += -I$(LX_CONTRIB_DIR)/drivers/usb/chipidea
+CC_OPT_dummies                     += -I$(LX_CONTRIB_DIR)/drivers/usb/chipidea
+
+#
+# Genode C-API backends
+#
+
+SRC_CC  += genode_c_api/usb.cc
+
+vpath genode_c_api/usb.cc $(subst /genode_c_api,,$(call select_from_repositories,src/lib/genode_c_api))
+
+# Driver-specific device-tree binary data
+BOARDS                     := iot_gate
+DTS_PATH(iot_gate)         := arch/arm64/boot/dts/compulab/sb-iotgimx8-ied.dts
+DTS_EXTRACT(iot_gate)      := --select usbotg1 --select usbotg2
+
+include $(REP_DIR)/src/drivers/linux_mnt_reform2_drv_target_arm_v8.inc
