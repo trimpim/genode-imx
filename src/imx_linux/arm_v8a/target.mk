fedbb16c08caa14e28b2aa7d0f8fae271216ae75
Alice Domage <alice.domage@gapfruit.com>
2024-04-12 09:59:43 +0200

    imx8mp_iot_gate: add support for minimal Linux 6.1
    Issue gapfruit#2082

--- src/imx_linux/arm_v8a/target.mk
+++ src/imx_linux/arm_v8a/target.mk
@@ -6,6 +6,7 @@ CUSTOM_TARGET_DEPS := kernel_build.phony
 LX_DIR := $(call select_from_ports,linux-imx)/linux-imx
 PWD    := $(shell pwd)
 
+## To enable debug log, kernel wide, add -> EXTRA_CFLAGS=-DDEBUG
 LX_MK_ARGS = ARCH=arm64 CROSS_COMPILE=$(CROSS_DEV_PREFIX)
 
 #
@@ -25,6 +26,9 @@ kernel_config.tag:
 	$(VERBOSE)$(MAKE) -C $(LX_DIR) O=$(PWD) $(LX_MK_ARGS) tinyconfig $(BUILD_OUTPUT_FILTER)
 	$(VERBOSE)$(LX_DIR)/scripts/config $(addprefix --enable ,$(LX_ENABLE))
 	$(VERBOSE)$(LX_DIR)/scripts/config $(addprefix --disable ,$(LX_DISABLE))
+	# Log verbosity can be adjusted here, see include/linux/printk.h
+	$(VERBOSE)$(LX_DIR)/scripts/config --set-val MESSAGE_LOGLEVEL_DEFAULT 7
+	$(VERBOSE)$(LX_DIR)/scripts/config --set-val CONSOLE_LOGLEVEL_DEFAULT 10 
 	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) olddefconfig $(BUILD_OUTPUT_FILTER)
 	$(VERBOSE)touch $@
 
@@ -33,5 +37,11 @@ kernel_config.tag: $(MAKEFILE_LIST)
 
 kernel_build.phony: kernel_config.tag
 	$(MSG_BUILD)Linux
-	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) dtbs Image $(BUILD_OUTPUT_FILTER)
+	## TODO: re-enable dtbs option, and fix issues with compulab dtb
+	## $(VERBOSE)$(MAKE) $(LX_MK_ARGS) dtbs Image Image.gz $(BUILD_OUTPUT_FILTER)
+	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) Image Image.gz $(BUILD_OUTPUT_FILTER)
+	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) compulab/iot-gate-imx8plus-m2tpm.dtb $(BUILD_OUTPUT_FILTER)
+	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) freescale/imx8mq-evk.dtb $(BUILD_OUTPUT_FILTER)
+	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) freescale/imx8mq-mnt-reform2.dtb $(BUILD_OUTPUT_FILTER)
+	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) freescale/imx8mq-mnt-reform2-hdmi.dtb $(BUILD_OUTPUT_FILTER)
 
