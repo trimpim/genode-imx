48f94b0820966ecc879e1421234d1724fa69f861
Pirmin Duss <pirmin.duss@gapfruit.com>
2024-10-04 08:06:33 +0200

    fixup! vfs_i2c-imx: VFS plugin for i2c on IMX SOCs

--- src/lib/vfs/i2c/imx/driver_imx.cc
+++ src/lib/vfs/i2c/imx/driver_imx.cc
@@ -2,7 +2,7 @@
  *  \brief  VFS plugin interface implementation
  *  \author Alice Domage
  *  \author Pirmin Duss
- *  \date   2022-06-28
+ *  \date   2024-09-28
  */
 
 /*
@@ -15,9 +15,11 @@
 /* Genode includes */
 #include <base/mutex.h>
 #include <irq_session/client.h>
+#include <os/vfs.h>
 #include <platform_session/connection.h>
-#include <timer_session/connection.h>
 #include <platform_session/device.h>
+#include <timer_session/connection.h>
+#include <util/list.h>
 
 /* i2c includes */
 #include <i2c/driver_base.h>
@@ -31,6 +33,7 @@ namespace I2c_imx
 	using namespace Genode;
 
 	class Driver;
+	class Driver_element;
 }
 
 
@@ -38,19 +41,18 @@ namespace {
 
 	Genode::uint8_t _bus_speed_to_divider(Genode::uint16_t bus_speed_khz)
 	{
-	   /* the table can be found:
-	    * IMX8MMRM.pdf on 5233
-	    *
-	    * The bus base frequency is 25MHz.
-	    */
+		/* the table can be found:
+		 * IMX8MMRM.pdf on page 5233
+		 *
+		 * The bus base frequency is 25MHz.
+		 */
+		if (bus_speed_khz >= 400) return 0x2a;   /* divide by   64 maximal speed supported */
+		if (bus_speed_khz >= 200) return 0x2f;   /* divide by  128 */
+		if (bus_speed_khz >= 100) return 0x33;   /* divide by  256 */
+		if (bus_speed_khz >=  50) return 0x37;   /* divide by  512 */
+		if (bus_speed_khz >=  25) return 0x3B;   /* divide by 1024 */
 
-	   if (bus_speed_khz >= 400) return 0x2a;   /* divide by   64 maximal speed supported */
-	   if (bus_speed_khz >= 200) return 0x2f;   /* divide by  128 */
-	   if (bus_speed_khz >= 100) return 0x33;   /* divide by  256 */
-	   if (bus_speed_khz >=  50) return 0x37;   /* divide by  512 */
-	   if (bus_speed_khz >=  25) return 0x3B;   /* divide by 1024 */
-
-	   return 0x3F;  /* divide by 2048 minimal speed */
+		return 0x3F;  /* divide by 2048 minimal speed */
 	}
 }
 
@@ -59,7 +61,8 @@ class I2c_imx::Driver final : public I2c::Driver_base
 {
 	private:
 
-		Env   &_env;
+		Env                      &_env;
+		Vfs::Env::User           &_vfs_user;
 
 		Platform::Connection      _platform         { _env };
 		Platform::Device          _device           { _platform };
@@ -79,6 +82,7 @@ class I2c_imx::Driver final : public I2c::Driver_base
 		void _bus_busy();
 
 		void _wait_for_irq();
+
 		void _irq_handle() { _sem_cnt = 0; }
 
 		void _write(I2c::Bus_address &, I2c::Message&);
@@ -86,12 +90,20 @@ class I2c_imx::Driver final : public I2c::Driver_base
 
 	public:
 
-		Driver(Env &env, uint16_t bus_speed_khz, bool verbose) :
+		Driver(Env &env, Vfs::Env::User &vfs_user, I2c::Settings const &settings)
+		:
 			Driver_base     { },
 			_env            { env },
-			_bus_speed_khz  { bus_speed_khz },
-			_verbose        { verbose }
-		{ }
+			_vfs_user       { vfs_user },
+			_bus_speed_khz  { settings.bus_speed_khz },
+			_verbose        { settings.verbose }
+		{
+			_irq.sigh(_irq_handler);
+
+			_irq_handle();
+			_irq.ack();
+			_bus_reset();
+		}
 
 		void transfer(I2c::Bus_address &a, I2c::Transaction &t) override;
 };
@@ -100,12 +112,16 @@ class I2c_imx::Driver final : public I2c::Driver_base
 void I2c_imx::Driver::_wait_for_irq()
 {
 	using I2c::Mmio;
+warning(__func__,"()  ::  ");
 
 	_sem_cnt++;
-	while (_sem_cnt > 0)
+	while (_sem_cnt > 0) {
+//		_vfs_user.wakeup_vfs_user();
 		_env.ep().wait_and_dispatch_one_io_signal();
+	}
 
 	if (_mmio.read<Mmio::Control::Master_slave_select>() == 0) {
+warning(__func__,"()  ::  ");
 		_bus_stop();
 		if (_verbose) {
 			error("Arbitration lost on bus ");
@@ -123,6 +139,7 @@ void I2c_imx::Driver::_bus_busy()
 	while (!_mmio.read<Mmio::Status::Busy>()) {
 		uint64_t const current = _timer.elapsed_ms();
 		if (current - start_time > 1000) {
+warning(__func__,"()  ::  ");
 			_bus_stop();
 			if (_verbose) {
 				error("Timeout on bus");
@@ -145,16 +162,19 @@ void I2c_imx::Driver::_bus_reset()
 void I2c_imx::Driver::_bus_start()
 {
 	using I2c::Mmio;
+warning(__func__,"()  ::  ","   enter");
 
 	/* input root 90 is 25Mhz select divisor to approximate desired bus speed */
 	_mmio.write<Mmio::Freq_divider>(_bus_speed_to_divider(_bus_speed_khz));
 	_mmio.write<Mmio::Status>(0);
 	_mmio.write<Mmio::Control>(Mmio::Control::Enable::bits(1));
 
+Genode::log("   *************  ", Hex {  _mmio.read<Mmio::Freq_divider>() });
 	uint64_t const start_time = _timer.elapsed_ms();
 	while (!_mmio.read<Mmio::Control::Enable>()) {
 		uint64_t const current = _timer.elapsed_ms();
 		if (current - start_time > 1000) {
+warning(__func__,"()  ::  ",__LINE__);
 			_bus_stop();
 			if (_verbose) {
 				error("Timeout on bus");
@@ -174,6 +194,7 @@ void I2c_imx::Driver::_bus_start()
 	                           Mmio::Control::Enable::bits(1));
 
 	_mmio.write<Mmio::Status::Ial>(0);
+warning(__func__,"()  ::  ","   leave");
 }
 
 
@@ -188,6 +209,7 @@ void I2c_imx::Driver::_bus_stop()
 void I2c_imx::Driver::_bus_write(uint8_t data)
 {
 	using I2c::Mmio;
+warning(__func__,"()  ::  ",Hex { data, Hex::Prefix::PREFIX, Hex::Pad::PAD  });
 
 	_mmio.write<Mmio::Data>(data);
 
@@ -197,7 +219,10 @@ void I2c_imx::Driver::_bus_write(uint8_t data)
 	_mmio.write<Mmio::Status::Irq>(0);
 	_irq.ack();
 
-	if (_mmio.read<Mmio::Status::Rcv_ack>()) {
+auto x { _mmio.read<Mmio::Status::Rcv_ack>() };
+//	if (_mmio.read<Mmio::Status::Rcv_ack>()) {
+	if (x) {
+warning(__func__,"()  ::  ","  >>>  ",Hex { x });
 		_bus_stop();
 		if (_verbose) {
 			error("Slave did not acknowledge");
@@ -207,17 +232,24 @@ void I2c_imx::Driver::_bus_write(uint8_t data)
 }
 
 
-void I2c_imx::Driver::_write(I2c::Bus_address  &bus_address, I2c::Message & m)
+void I2c_imx::Driver::_write(I2c::Bus_address  &bus_address, I2c::Message &m)
 {
+warning(__func__,"()  ::  1");
+
 	/* LSB must be 0 for writing on the bus, address is on the 7 hightest bits */
-	_bus_write(bus_address.address << 1);
-	m.for_each([&] (unsigned, uint8_t & byte) { _bus_write(byte); });
+	_bus_write((uint8_t)(bus_address.address << 1));
+warning(__func__,"()  ::  2");
+
+	m.for_each([&] (unsigned, uint8_t &byte) {
+log("  >>>  ",Hex { byte });
+		/*_bus_write(byte);*/ });
 }
 
 
-void I2c_imx::Driver::_read(I2c::Bus_address  &bus_address, I2c::Message & m)
+void I2c_imx::Driver::_read(I2c::Bus_address  &bus_address, I2c::Message &m)
 {
 	using I2c::Mmio;
+warning(__func__,"()  ::  ");
 
 	/* LSB must be 1 for reading on the bus, address is on the 7 hightest bits */
 	_bus_write((uint8_t)(bus_address.address << 1 | 1));
@@ -249,9 +281,10 @@ void I2c_imx::Driver::_read(I2c::Bus_address  &bus_address, I2c::Message & m)
 }
 
 
-void I2c_imx::Driver::transfer(I2c::Bus_address &a, I2c::Transaction &t)
+void I2c_imx::Driver::transfer(I2c::Bus_address &address, I2c::Transaction &t)
 {
 	using I2c::Mmio;
+warning(__func__,"()  ::  ");
 
 	_bus_start();
 
@@ -261,17 +294,47 @@ void I2c_imx::Driver::transfer(I2c::Bus_address &a, I2c::Transaction &t)
 			_bus_busy();
 		}
 
-		if (m.type == I2c::Message::READ) { _read(a, m);
-		} else { _write(a, m); }
+		if (m.type == I2c::Message::READ) { _read(address, m);
+		} else { _write(address, m); }
 	});
 
 	_bus_stop();
 }
 
 
-I2c::Driver_base &_create_driver_instance(Genode::Env &env, I2c::Settings)
+class I2c_imx::Driver_element final : public List<Driver_element>::Element
 {
-	static I2c_imx::Driver driver { env, 10, true};
+	private:
 
-	return driver;
+		Driver           _driver;
+		I2c::Name const  _name;
+
+	public:
+
+		Driver_element(Env &env, Vfs::Env::User &vfs_user, I2c::Settings const &settings)
+		:
+			_driver { env, vfs_user, settings },
+			_name   { settings.name }
+		{ }
+
+		Driver             &driver()        { return _driver; }
+		I2c::Name   const  &name()    const { return _name; }
+};
+
+
+I2c::Driver_base &_create_driver_instance(Genode::Env &env, Genode::Allocator &alloc,
+                                          Vfs::Env::User &vfs_user, I2c::Settings const &settings)
+{
+	static Genode::List<I2c_imx::Driver_element> drivers;
+
+	for (I2c_imx::Driver_element *d = drivers.first() ; d ; d = d->next()) {
+		if (settings.name == d->name()) {
+			return (d->driver());
+		}
+	}
+
+	I2c_imx::Driver_element *driver { new (alloc) I2c_imx::Driver_element { env, vfs_user, settings } };
+	drivers.insert(driver);
+
+	return (driver->driver());
 }
